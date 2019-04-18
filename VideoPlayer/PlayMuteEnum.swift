810c54447033efeb2c1bae8a18a697ecd4f5d924
雨夜 <kiras0518@gmail.com>
2019-04-18 17:23:20 +0800

    init  PlayMuteEnum

---
+++ VideoPlayer/PlayMuteEnum.swift
@@ -0,0 +1,24 @@
+//
+//  PlayMuteEnum.swift
+//  VideoPlayer
+//
+//  Created by YU on 2019/4/18.
+//  Copyright © 2019 ameyo. All rights reserved.
+//
+
+import Foundation
+
+enum PlayStatus {
+    
+    case play
+    
+    case stop
+}
+
+enum SoundStatus {
+    
+    case mute
+    
+    case unmute
+    
+}
